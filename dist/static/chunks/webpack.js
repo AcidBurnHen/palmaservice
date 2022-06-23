96aa1773ea740502362668e1549db3fcd2eb3686
acidburn <cc.acidburn@gmail.com>
2022-06-23 04:09:26 +0200

    Fixed image height overlapping album selector

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f1c5958c1991903c"; }
+/******/ 		__webpack_require__.h = function() { return "23362f0a5ed8221a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
